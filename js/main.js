ad42e9c4667641afafb986ba55521a4ae573127f
SBickram <stevebickram@gmail.com>
2015-03-19 01:26:58 -0400

    inputting of js code
    smooth scrolling and waypoints of two elements.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ var $btnCollapse = $('.btn-collapse-expand')
 var $box = $('.box');
 
 $btnCollapse.on('click', function () {
-    $box.toggleClass('js-collapse');
-    
-});
+    $box.toggleClass('js-collapse');    
+});;
+
+var $intro = $('.intro');
+
+$intro.waypoint(function () {
+    $intro.addClass('js-intro-animate');
+}, { offset: '50%'});
+
+var $gallery = $('.gallery');
+
+$gallery.waypoint(function () {
+    $gallery.addClass('js-gallery-animate');
+}, { offset: '50%'});
